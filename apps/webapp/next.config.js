29aa654dbb1ba931e4fb2e2ad22144482cc01816
Dragos-Alexandru Gabor <gabor.dragos@gmail.com>
2023-04-03 17:14:52 +0200

    productImage/webapp: changed next.config.js to allow usage of image resources from api webserver
    Signed-off-by: Dragos-Alexandru Gabor <gabor.dragos@gmail.com>

--- apps/webapp/next.config.js
+++ apps/webapp/next.config.js
@@ -3,6 +3,14 @@ const withTM = require("next-transpile-modules")(["database"]);
 /** @type {import('next').NextConfig} */
 const nextConfig = withTM({
   reactStrictMode: true,
+  images: {
+	remotePatterns: [
+		{
+			protocol: 'http',
+			hostname: 'localhost'
+		}
+	]
+  }
 });
 
 module.exports = nextConfig;
