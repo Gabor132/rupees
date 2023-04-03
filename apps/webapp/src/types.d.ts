f01c000d23d13c680c559531a25483af9d1abf06
Dragos-Alexandru Gabor <gabor.dragos@gmail.com>
2023-04-03 17:23:38 +0200

    productImage: add ImageUploadResult type
    Signed-off-by: Dragos-Alexandru Gabor <gabor.dragos@gmail.com>

--- apps/webapp/src/types.d.ts
+++ apps/webapp/src/types.d.ts
@@ -2,6 +2,17 @@ import { Product, User as PrismaUser } from "database";
 
 export type Rupee = 5 | 10 | 20 | 50 | 100;
 
+export type ImageUploadResult = {
+  destination: string;
+  encoding: string;
+  fieldname: string;
+  filename: string;
+  mimeType: string;
+  originalName: string;
+  path: string;
+  size: number;
+};
+
 export type BuyResult = {
   totalSpent: number;
   product: Product;
